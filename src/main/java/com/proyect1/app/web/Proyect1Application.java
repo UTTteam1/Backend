98dbf20806f0fcd6fecede91ea5710d08b87ae48
Chio2909 <rozzi.gmd@gmail.com>
2022-09-28 23:19:21 -0500

    nuevo

---
+++ src/main/java/com/proyect1/app/web/Proyect1Application.java
@@ -0,0 +1,13 @@
+package com.proyect1.app.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Proyect1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Proyect1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/proyect1/app/web/Proyect1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyect1.app.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Proyect1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
